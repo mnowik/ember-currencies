c344f48ec455a23b5a6f92f12c1a0ffd2dccdd62
mnowik <nowicki.matthieu@gmail.com>
2017-07-10 13:44:40 +0300

    add properties and helpers

--- addon/index.js
+++ addon/index.js
@@ -1,13 +1,16 @@
-import { CURRENCIES_LIST, CURRENCIES_SYMBOLS_LIST, CURRENCIES_PER_COUNTRY_LIST } from './utils/currencies-list';
+import { CURRENCIES_LIST, CURRENCIES_SYMBOLS_LIST, CURRENCIES_PER_COUNTRY_LIST } from './utils/currencies-lists';
+import { getCurrency } from './utils/currencies-properties';
 
 export {
   CURRENCIES_LIST,
   CURRENCIES_SYMBOLS_LIST,
-  CURRENCIES_PER_COUNTRY_LIST
+  CURRENCIES_PER_COUNTRY_LIST,
+  getCurrency
 };
 
 export default {
   CURRENCIES_LIST,
   CURRENCIES_SYMBOLS_LIST,
-  CURRENCIES_PER_COUNTRY_LIST
+  CURRENCIES_PER_COUNTRY_LIST,
+  getCurrency
 };
--- addon/utils/currencies-properties.js
+++ addon/utils/currencies-properties.js
@@ -1,2 +1,46 @@
 import Em from 'ember';
 import { CURRENCIES_LIST, CURRENCIES_SYMBOLS_LIST, CURRENCIES_PER_COUNTRY_LIST } from './currencies-lists';
+import { getFormat, buildIndex } from './helpers';
+
+const {
+  isNone
+} = Em;
+
+let indexedByIso3;
+let indexedByIsoNumeric;
+
+
+export function getCurrency(value) {
+  let valueFormat = getFormat(value);
+  if (valueFormat === null) {return;}
+
+  let listIndexed = _getCurrenciesListIndexed(valueFormat);
+  let selectedCountry = listIndexed[value.toUpperCase()];
+
+  if (!isNone(selectedCountry)) {
+    return selectedCountry;
+  }
+}
+
+
+// export function getCurrencySymbol(value) {
+
+// }
+
+
+// export function getCurrencyOfCountry(value) {
+
+// }
+
+
+function _getCurrenciesListIndexed(code) {
+  let index = {};
+  if (code === 'iso3') {
+    index = buildIndex(CURRENCIES_LIST, code, indexedByIso3);
+    indexedByIso3 = index;
+  } else if (code === 'isoNumeric') {
+    index = buildIndex(CURRENCIES_LIST, code, indexedByIsoNumeric);
+    indexedByIsoNumeric = index;
+  }
+  return index;
+}
